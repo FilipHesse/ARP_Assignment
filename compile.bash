67a4fc4ef3ef1822483a4ff48a13223b3676aeb4
Filip Hesse <Filip_Hesse@yahoo.de>
2020-02-16 22:57:11 +0100

    Compile and run using bash
    gitignore ignores compiled files in Release folder

---
+++ compile.bash
@@ -0,0 +1,5 @@
+g++ -o Release/processP -std=c++11 processP.cpp
+g++ -o Release/processS -std=c++11 processS.cpp
+g++ -o Release/processL -std=c++11 processL.cpp
+g++ -o Release/processG -std=c++11 processG.cpp
+g++ -o Release/multi_process -std=c++11 multi_process.cpp
---
+++ run.bash
@@ -0,0 +1,1 @@
+./Release/multi_process
